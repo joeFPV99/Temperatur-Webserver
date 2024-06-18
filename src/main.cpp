ac8540a460f3c43d8a94387795ee5a574ee6d890
JonasLudwig <joul99@web.de>
2024-06-18 07:52:29 +0200

    added libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <DHT.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-// put function declarations here:
-int myFunction(int, int);
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+ 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ 
+ 
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+
