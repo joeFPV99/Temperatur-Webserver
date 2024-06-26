fa0b36bd2ca93bda4187c4c792a422d80fd5fa26
JonasLudwig <joul99@web.de>
2024-06-26 06:57:30 +0200

    removed header file

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,17 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+#define DHTPIN 5 //TBD!
+#define DHTTYPE DHT11
+
+//connstruct DHT obj
+DHT dht(DHTPIN,DHTTYPE);
+
+//construct Adafruit OLED obj
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
 void setup() {
@@ -12,7 +23,7 @@ void setup() {
 
 void loop() {
  
- 
+
 }
 
 
